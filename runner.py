9d72ef4c67f2eea384a395e12e48d1961cb69689
padawanjonbob <padawanjonbob@gmail.com>
2026-04-14 02:05:00 -0400

    Add files via upload

--- runner.py
+++ runner.py
@@ -5,6 +5,7 @@ import threading
 class Runner:
     def __init__(self, controller):
         self.app = controller
+        self.proc = None
 
     @property
     def ui(self):
@@ -18,20 +19,30 @@ class Runner:
 
     def execute(self):
         try:
-            proc = subprocess.Popen(
+            self.proc = subprocess.Popen(
                 ["cyberbasic.exe", self.app.file_path],
                 stdout=subprocess.PIPE,
                 stderr=subprocess.STDOUT,
-                text=True
+                text=True,
+                encoding="utf-8",
+                errors="replace"
             )
 
-            for line in proc.stdout:
+            for line in iter(self.proc.stdout.readline, ''):
+                if not line:
+                    break
                 self.log(line)
+
+            self.proc.wait()
+            self.log(f"\nProcess exited with code {self.proc.returncode}\n")
 
         except Exception as e:
             self.log(str(e))
 
     def log(self, msg):
+        self.ui.console.after(0, self._log_ui, msg)
+
+    def _log_ui(self, msg):
         self.ui.console.config(state="normal")
         self.ui.console.insert("end", msg)
         self.ui.console.see("end")
